6d7343f19f4856120badbe578933e86246950418
anouns <Kirillperekatnov14@gmail.com>
2026-03-21 01:41:49 +0300

    Fix startup crash: standard plugin initialization in lib.rs

--- vey-v2/src-tauri/src/lib.rs
+++ vey-v2/src-tauri/src/lib.rs
@@ -1,15 +1,11 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .setup(|app| {
-      app.handle().plugin(tauri_plugin_dialog::init())?;
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
+    .plugin(tauri_plugin_dialog::init())
+    .plugin(tauri_plugin_log::Builder::default()
+        .level(log::LevelFilter::Info)
+        .build())
+    .setup(|_app| {
       Ok(())
     })
     .run(tauri::generate_context!())
